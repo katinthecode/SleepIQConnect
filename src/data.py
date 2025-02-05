f34d52512ecf37edaafb816f75dd5e68827ec5bd
Katie Antlitz <katinthecode@gmail.com>
2025-02-04 19:34:10 -0600

    Adding Responsive Air get/set

--- src/data.py
+++ src/data.py
@@ -77,3 +77,31 @@ class Sleeper:
                     Account ID: {self.account_id}
                     Last Login: {self.last_login}
                     Side: {self.side}'''
+                    
+@dataclass
+class Responsive_Air_Settings:
+    adjustment_threshold: int
+    in_bed_timeout: int
+    left_side_enabled: bool
+    right_side_enabled: bool
+    out_of_bed_timeout: int
+    poll_frequency: int
+    pref_sync_state: str
+
+    def __init__(self, json):
+        self.adjustment_threshold = json['adjustmentThreshold']
+        self.in_bed_timeout = json['inBedTimeout']
+        self.left_side_enabled = json['leftSideEnabled']
+        self.right_side_enabled = json['rightSideEnabled']
+        self.out_of_bed_timeout = json['outOfBedTimeout']
+        self.poll_frequency = json['pollFrequency']
+        self.pref_sync_state = json['prefSyncState']
+        
+    def __str__(self):
+        return f'''Adjustment Threshold: {self.adjustment_threshold}
+                    In Bed Timeout: {self.in_bed_timeout}
+                    Left Side Enabled: {self.left_side_enabled}
+                    Right Side Enabled: {self.right_side_enabled}
+                    Out Of Bed Timeout: {self.out_of_bed_timeout}
+                    Poll Frequency: {self.poll_frequency}
+                    Pref Sync State: {self.pref_sync_state}'''
--- src/sleep_iq_api.py
+++ src/sleep_iq_api.py
@@ -4,7 +4,7 @@ import requests
 
 from dotenv import load_dotenv
 
-from data import Authentication_Login, Authentication_Token, Sleeper
+from data import Authentication_Login, Authentication_Token, Responsive_Air_Settings, Sleeper
 
 load_dotenv()
 
@@ -46,13 +46,13 @@ def get_authorization_login():
         
     return Authentication_Login(response.json(), response.cookies)
 
-def get_sleepers(auth):
+def get_sleepers(auth_token):
     sleeper_endpoint = '/sleeper'
     sleepers = []
     
     payload = {}
     headers = {
-        'Authorization': auth.access_token
+        'Authorization': auth_token.access_token
     }
     
     response = requests.request('GET', f'{os.getenv('URL')}{sleeper_endpoint}', headers=headers, data=json.dumps(payload))
@@ -70,6 +70,46 @@ def get_sleepers(auth):
         
     return sleepers
 
+def get_responsive_air_data(auth_login: Authentication_Login, bed_id: str):
+    responsive_air_endpoint = f'/bed/{bed_id}/responsiveAir?_k={auth_login.key}'
+    payload = {}
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request('GET', f'{os.getenv('URL')}{responsive_air_endpoint}', 
+                                headers=headers, data=payload, cookies=auth_login.cookies)
+    
+    if response.status_code < 200 or response.status_code >= 300:
+        print('Error: ' + str(response.status_code))
+        print(response.text)
+        exit()
+        
+    return Responsive_Air_Settings(response.json())
+
+def set_responsive_air_data(auth_login: Authentication_Login, bed_id: str):
+    responsive_air_endpoint = f'/bed/{bed_id}/responsiveAir?_k={auth_login.key}'
+    payload = {
+        'EnabledAuto': 'enabled'
+    }
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request('PUT', f'{os.getenv('URL')}{responsive_air_endpoint}', 
+                                headers=headers, data=json.dumps(payload), cookies=auth_login.cookies)
+    
+    if response.status_code < 200 or response.status_code >= 300:
+        print('Error: ' + str(response.status_code))
+        print(response.text)
+        exit()
+        
+    print(response.text)
+
 if __name__ == '__main__':
-    auth = get_authorization_login()
-    print(str(auth))
+    auth_token = get_authorization_token()
+    sleepers = get_sleepers(auth_token)
+    auth_login = get_authorization_login()
+    set_responsive_air_data(auth_login, sleepers[0].bed_id)
+    
